12979cd6f2c3a5aa39cb67a28bd7774e5ae22a85
nikola@nikola.nikola <nikola@nikola.nikola>
2022-08-13 12:51:58 -0700

    completed login form

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,8 +1,33 @@
 import React, { useState } from 'react'
+import { Stack } from '@mui/system'
+import { TextField, Typography, Button } from '@mui/material'
+import { auth } from '../firebase'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
     const [loginInfo, setLoginInfo] = useState({ email: "", password: "" })
+
+    const navigate = useNavigate()
+
+    const handleLogin = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(auth, loginInfo.email, loginInfo.password)
+            navigate("/calendar")
+        }
+        catch(error) {
+            alert(error.message)
+        }
+    }
+
     return (
-        <div>Login</div>
+        <div>
+        <Stack spacing={2} direction="column">
+            <Typography align="center" variant="h2">Log Into Your Account</Typography>
+            <TextField required label="Email" variant="filled" type="email" value={loginInfo.email} onChange={(e) => setLoginInfo({...loginInfo, email: e.target.value})} />
+            <TextField required label="Password" variant="filled" type="password" value={loginInfo.password} onChange={(e) => setLoginInfo({...loginInfo, password: e.target.value})} /> 
+            <Button variant="contained" onClick={() => handleLogin()}>Log In</Button>
+        </Stack>
+    </div>
     )
 }
